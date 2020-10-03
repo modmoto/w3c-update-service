3b549cc517907221f9a86843524dc4653a342f2f
Alexander Beninski <abeninskibede>
2020-10-03 16:00:21 +0300

    use releases from github for maps and webui

--- w3c-update-service/VersionController.cs
+++ w3c-update-service/VersionController.cs
@@ -15,8 +15,6 @@ namespace w3c_update_service
     [Route("api")]
     public class VersionController : ControllerBase
     {
-        private string _updateFileFolder = "UpdateFiles/";
-
         private readonly IHttpClientFactory _clientFactory;
 
         public VersionController(IHttpClientFactory clientFactory)
@@ -25,24 +23,46 @@ namespace w3c_update_service
         }
 
         [HttpGet("client-version")]
-        public IActionResult GetVersion()
+        public async Task<IActionResult> GetVersion()
         {
-            var latestMapData = GetLatestMapsData();
-            return Ok(new { version = latestMapData.Version.ToString() });
+            var latestRelease = await GetLatestUpdateServiceReleaseFromGithub();
+
+            if (latestRelease == null)
+            {
+                return BadRequest("There was a problem getting data from github");
+            }
+
+            return Ok(new { version = latestRelease.Name });
         }
 
         [HttpGet("maps")]
-        public IActionResult GetMaps()
+        public async Task<IActionResult> GetMaps()
         {
-            var latestMapData = GetLatestMapsData();
+            var latestRelease = await GetLatestUpdateServiceReleaseFromGithub();
 
-            return LoadFileDirectly(latestMapData.FilePath, "maps");
+            if (latestRelease == null)
+            {
+                return BadRequest("There was a problem getting data from github");
+            }
+
+            var url = GetLinkToReleaseAssetByFileName(latestRelease, "maps");
+
+            return Redirect(url);
         }
 
         [HttpGet("webui")]
-        public IActionResult GetWebUi(bool ptr)
+        public async Task<IActionResult> GetWebUi(bool ptr)
         {
-            return LoadFile(_updateFileFolder, ptr ? "ptr-webui" : "webui");
+            var latestRelease = await GetLatestUpdateServiceReleaseFromGithub();
+
+            if (latestRelease == null)
+            {
+                return BadRequest("There was a problem getting data from github");
+            }
+
+            var url = GetLinkToReleaseAssetByFileName(latestRelease, ptr ? "ptr-webui" : "webui");
+
+            return Redirect(url);
         }
 
 
@@ -72,7 +92,7 @@ namespace w3c_update_service
                    return BadRequest("Unsupported OS Version");
             }
 
-            var url = GetLinkToReleaseAsset(latestRelease, fileExtension);
+            var url = GetLinkToReleaseAssetByFileExtension(latestRelease, fileExtension);
 
             return Redirect(url);
         }
@@ -90,57 +110,33 @@ namespace w3c_update_service
             return Ok(new { version = latestRelease.Name });
         }
 
-        private static string GetLinkToReleaseAsset(GithubReleaseResponse response, string fileExtension)
+        private static string GetLinkToReleaseAssetByFileExtension(GithubReleaseResponse response, string fileExtension)
         {
             var releaseAsset = response.Assets.FirstOrDefault(x => x.Name.EndsWith(fileExtension));
 
             return releaseAsset?.BrowserDownloadUrl;
         }
 
-        private static IActionResult LoadFile(string basePath, string fileNameStart)
+        private static string GetLinkToReleaseAssetByFileName(GithubReleaseResponse response, string startsWithFileName)
         {
-            var filePath = Directory.GetFiles(basePath)
-                .Where(f => f.StartsWith(basePath + fileNameStart))
-                .OrderByDescending(f => f)
-                .First();
-            var dataBytes = System.IO.File.ReadAllBytes(filePath);
-            var fileContentResult = new FileContentResult(dataBytes, "application/zip");
-            fileContentResult.FileDownloadName = $"{fileNameStart}.zip";
-            return fileContentResult;
-        }
+            var releaseAsset = response.Assets.FirstOrDefault(x => x.Name.StartsWith(startsWithFileName));
 
-        private static IActionResult LoadFileDirectly(string filePath, string downloadFileName)
-        {
-            var dataBytes = System.IO.File.ReadAllBytes(filePath);
-            var fileContentResult = new FileContentResult(dataBytes, "application/zip");
-            fileContentResult.FileDownloadName = $"{downloadFileName}.zip";
-            return fileContentResult;
-        }
-
-        private MapsData GetLatestMapsData()
-        {
-            var mapFiles = Directory.GetFiles(_updateFileFolder)
-             .Where(f => f.StartsWith(_updateFileFolder + "maps"));
-
-            List<MapsData> mapsData = new List<MapsData>();
-            foreach (var mapFile in mapFiles)
-            {
-                int version = 0;
-
-                if(int.TryParse(mapFile.Split("_v")[1].Replace(".zip", ""), out version))
-                {
-                    var mapData = new MapsData() { FilePath = mapFile, Version = version };
-                    mapsData.Add(mapData);
-                }
-            }
-
-            return mapsData.OrderByDescending(x => x.Version).First();
+            return releaseAsset?.BrowserDownloadUrl;
         }
 
         private async Task<GithubReleaseResponse> GetLatestLauncherReleaseFromGithub()
         {
-            var request = new HttpRequestMessage(HttpMethod.Get,
-            "https://api.github.com/repos/w3champions/w3champions-launcher/releases/latest");
+            return await GetLatestReleaseFromRepo("w3champions/w3champions-launcher");
+        }
+
+        private async Task<GithubReleaseResponse> GetLatestUpdateServiceReleaseFromGithub()
+        {
+            return await GetLatestReleaseFromRepo("w3champions/w3champions-update-service");
+        }
+
+        private async Task<GithubReleaseResponse> GetLatestReleaseFromRepo(string repoName)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.github.com/repos/{repoName}/releases/latest");
             request.Headers.Add("Accept", "application/vnd.github.v3+json");
             request.Headers.Add("User-Agent", "Asp.WebApi");
 
@@ -155,39 +151,6 @@ namespace w3c_update_service
             }
 
             return null;
-        }
-    }
-
-    class UpdateTo : IComparable<UpdateTo>
-    {
-        public int Patch { get; }
-        public int Minor { get; }
-        public int Major { get; }
-        public string Path { get; }
-
-        public UpdateTo(string path)
-        {
-            Path = path;
-
-            var split = path
-                .Split("/").Last()
-                .Replace(".exe", "")
-                .Replace(".dmg", "")
-                .Replace(".AppImage", "")
-                .Replace("w3champions Setup ", "")
-                .Replace("w3champions-", "")
-                .Split(".");
-
-            Patch = int.Parse(split[2]);
-            Minor = int.Parse(split[1]);
-            Major = int.Parse(split[0]);
-        }
-
-        public int CompareTo(UpdateTo other)
-        {
-            if (Major != other.Major) return Major - other.Major;
-            if (Minor != other.Minor) return Minor - other.Minor;
-            return Patch - other.Patch;
         }
     }
 
