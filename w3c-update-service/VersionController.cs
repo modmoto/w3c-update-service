efc6ef5172a6e06816247142431079a50e5ceb3d
Gab <gabriel@nunez.com>
2021-03-11 15:22:35 -0500

    v22

--- w3c-update-service/VersionController.cs
+++ w3c-update-service/VersionController.cs
@@ -15,7 +15,7 @@ namespace w3c_update_service
     public class VersionController : ControllerBase
     {
         private const int GithubReleaseCacheMunutes = 5;
-        private const int CurrentVersion = 21;
+        private const int CurrentVersion = 22;
 
         private static readonly string _launcherFolder = "Launchers";
 
